db601a39a6144d93ef016a30583217cbf296930a
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-07-31 22:01:29 +0530

    AWS part completed

--- karma_app/lib/main.dart
+++ karma_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-import 'home.dart'; // Import the home.dart file
+ // Import the home.dart file
 // main.dart
 import 'amplifyconfiguration.dart';
 
@@ -34,41 +34,143 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  // Function to navigate to the HomeScreen
+  final String userName = "USER_NAME"; // Replace with the user's name
+  final String avatarImageURL =
+      "https://example.com/avatar.jpg"; // Replace with the actual URL
 
-  void _goToHomeScreen() {
-  Navigator.pushNamed(context, '/home');
-}
+  final List<String> dates = List.generate(11, (index) {
+    DateTime date = DateTime.now().add(Duration(days: index));
+    return "${date.day}/${date.month}";
+  });
+
+  List<String> todos = []; // Initialize todos as an empty list
+  // Function to navigate to the HomeScreen
 
   @override
   Widget build(BuildContext context) {
     return Authenticator(
       child: MaterialApp(
         builder: Authenticator.builder(),
-        routes: {
-        '/': (context) => MyApp(), // The default route (home)
-        '/home': (context) => Home(username: 'Gopal Verma'), // Named route for Home widget
-      },
+       
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Main Screen'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Navigation Bar
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+            color: Colors.blue,
+            child: Row(
               children: [
-                Text('You are logged in!'),
-                SizedBox(height: 20),
-                ElevatedButton(
-                onPressed: () => Navigator.pushNamed(context, '/home'), // Navigate to /home named route
-                child: Text('Go to HomeScreen'),
-              )
-              ]
+                // Welcome Text
+                Text(
+                  "WELCOME",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-            
+                // Spacer to push the Avatar to the right
+                Spacer(),
+                // User Name
+                Text(
+                  userName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                // Avatar
+                CircleAvatar(
+                  radius: 32.0,
+                  backgroundImage: NetworkImage(avatarImageURL), // Load image from URL
+                ),
+              ],
             ),
+          ),
+          // Add other widgets for the main content of the home screen below
+          // ...
+          Expanded(
+            child: ListView.builder(
+              itemCount: dates.length,
+              itemBuilder: (context, index) {
+                final date = dates[index];
+                return ListTile(
+                  title: Text(date),
+                  onTap: () {
+                    _showTodoModal(context, date);
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showTodoModal(context, "Add New Todo");
+        },
+        child: Icon(Icons.add),
+      ),
           ),
         ),
       );
+  }
+
+
+void _showTodoModal(BuildContext context, String selectedDate) {
+    final _todoController = TextEditingController();
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Todo List for $selectedDate",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: _todoController,
+                decoration: InputDecoration(
+                  hintText: "Type your todo...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_todoController.text.isNotEmpty) {
+                    // Add the todo to the list
+                    setState(() {
+                      todos.add(_todoController.text);
+                    });
+
+                    // Clear the text field after adding the todo
+                    _todoController.clear();
+                  }
+                },
+                child: Text("Add Todo"),
+              ),
+              SizedBox(height: 16.0),
+              // Display todos below the button
+              if (todos.isNotEmpty)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: todos.map((todo) => Text(todo)).toList(),
+                ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
