0d58383eb98bc3f5f379a6a60158f26777c80515
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-08-03 15:54:27 +0530

    chat_ui and theme part added

--- karma_app/lib/home.dart
+++ karma_app/lib/home.dart
@@ -1,47 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:karma_app/todo_tile.dart';
+import 'amplifyconfiguration.dart';
+import 'package:amplify_flutter/amplify.dart';
 
-class HomePage extends StatefulWidget{
-  const HomePage({super.key});
 
+void main() => runApp(ProductivityApp());
+
+class ProductivityApp extends StatefulWidget {
   @override
-  State<HomePage> createState()=> _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>{
-
-  // list of todo tasks
-  List toDoList=[
-    ['Make tutorial', false],
-    ['Do Exercise', false],
-  ];
-
-  //checkbox was tapped
-  void checkBoxChanged(bool? value, int index){
-    setState(() {
-      toDoList[index][1]= !toDoList[index][1];
-    });
-  }
   
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor:Colors.white ,
-      appBar: AppBar(
-        title: Text('TO DO'),
-        elevation: 0,
-      ),
-      body: ListView.builder(
-        itemCount: toDoList.length,
-        itemBuilder: (context, index){
-          return ToDoTile(
-            taskName: toDoList[index][0],
-            taskCompleted:toDoList[index][1] ,
-            onChanged: (value) => checkBoxChanged,
-            );
-
-        },
-      ),
-    );
-  }
 }
--- karma_app/lib/main.dart
+++ karma_app/lib/main.dart
@@ -1,10 +1,19 @@
+// main.dart
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-import 'home.dart'
-// main.dart
+//import 'home.dart'; // Import the home.dart file
 import 'amplifyconfiguration.dart';
+
+// Amplify Flutter Packages
+//import 'package:amplify_datastore/amplify_datastore.dart';
+//import 'package:amplify_api/amplify_api.dart'; // UNCOMMENT this line after backend is deployed
+
+// Generated in previous step
+//import 'models/ModelProvider.dart';
+import 'package:karma/screens/welcome/welcome_screen.dart';
+import 'package:karma/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,146 +43,28 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  final String userName = "USER_NAME"; // Replace with the user's name
-  final String avatarImageURL =
-      "https://example.com/avatar.jpg"; // Replace with the actual URL
-
-  final List<String> dates = List.generate(11, (index) {
-    DateTime date = DateTime.now().add(Duration(days: index));
-    return "${date.day}/${date.month}";
-  });
-
-  List<String> todos = []; // Initialize todos as an empty list
   // Function to navigate to the HomeScreen
+
 
   @override
   Widget build(BuildContext context) {
     return Authenticator(
       child: MaterialApp(
         builder: Authenticator.builder(),
-       
         home: Scaffold(
-          body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          // Navigation Bar
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
-            color: Colors.blue,
-            child: Row(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // Welcome Text
-                Text(
-                  "WELCOME",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+                Text('You are logged in!'),
+                SizedBox(height: 20),
+              ]
                 ),
-                // Spacer to push the Avatar to the right
-                Spacer(),
-                // User Name
-                Text(
-                  userName,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  ),
-                ),
-                // Avatar
-                CircleAvatar(
-                  radius: 32.0,
-                  backgroundImage: NetworkImage(avatarImageURL), // Load image from URL
-                ),
-              ],
+            
             ),
           ),
-          // Add other widgets for the main content of the home screen below
-          // ...
-          Expanded(
-            child: ListView.builder(
-              itemCount: dates.length,
-              itemBuilder: (context, index) {
-                final date = dates[index];
-                return ListTile(
-                  title: Text(date),
-                  onTap: () {
-                    _showTodoModal(context, date);
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showTodoModal(context, "Add New Todo");
-        },
-        child: Icon(Icons.add),
-      ),
-          ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      theme: ThemeData(primarySwatch: Colors.yellow),
+        
         ),
       );
-  }
-
-
-void _showTodoModal(BuildContext context, String selectedDate) {
-    final _todoController = TextEditingController();
-
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                "Todo List for $selectedDate",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _todoController,
-                decoration: InputDecoration(
-                  hintText: "Type your todo...",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_todoController.text.isNotEmpty) {
-                    // Add the todo to the list
-                    setState(() {
-                      todos.add(_todoController.text);
-                    });
-
-                    // Clear the text field after adding the todo
-                    _todoController.clear();
-                  }
-                },
-                child: Text("Add Todo"),
-              ),
-              SizedBox(height: 16.0),
-              // Display todos below the button
-              if (todos.isNotEmpty)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: todos.map((todo) => Text(todo)).toList(),
-                ),
-            ],
-          ),
-        );
-      },
-    );
   }
 }
